7671aa528381d3eb2a161fc7460797e4029d5132
kyroslee <kyrosllk1996@gmail.com>
2018-03-30 03:12:50 +0800

    added media control panel

--- src/render/index.js
+++ src/render/index.js
@@ -1,6 +1,12 @@
 const p2p = require('./p2p.js');
 const AudioPlayer = require('./audioPlayer.js');
+const MediaControl = require('./MediaControl.js');
 
 const audioCtx = new AudioContext();
 
 const audioPlayer = new AudioPlayer({audioCtx});
+const mediaControl = new mediaControl({
+    playPauseButton: '#play-pause-button',
+    seekClickable: '#seek-clickable',
+    seekProgress: '#seek-progress'
+})
